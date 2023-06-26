3b233be4b62c2e8e94cddb2e5a14017766d092a1
Matt Petters <mcpetters@gmail.com>
2023-06-26 07:53:39 -0700

    update pyscript

--- send_message.py
+++ send_message.py
@@ -1,9 +1,15 @@
 import os
+import sys
 
 def send_imessage(script_path):
-    os.system(f"osascript {script_path}")
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    # Construct full path to the AppleScript file
+    full_script_path = os.path.join(script_dir, script_path)
+    
+    os.system(f"osascript {full_script_path}")
 
-# Replace with the path to your AppleScript file
-script_path = "/path/to/your/script.scpt"
+# Replace with the name of your AppleScript file
+script_path = "send_imessage.scpt"
 send_imessage(script_path)
 
